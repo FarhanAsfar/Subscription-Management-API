dc2ccddd11d00bd5e8e2e51cfd5ecc7bb8c267cd
arnob <farhanasfar21@gmail.com>
2025-04-30 20:48:36 +0600

    rewrite auth controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,20 +2,3 @@ import User from "../models/user.model.js"
 import jwt from "jsonwebtoken"
 
 
-const signUP = async(req, res) => {
-    try {
-        const {username, email, password} = req.body;
-
-        if([username, email, password].some((field) => field?.trim() == "")){
-            console.log("All fields are required");
-        }
-
-        const userExist = await User.findOne({email});
-
-        if(userExist){
-            console.log("User already exists");
-        }
-    } catch (error) {
-        console.error("Signup Failed", error);
-    }
-}
