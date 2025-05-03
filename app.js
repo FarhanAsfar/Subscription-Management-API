787fa29f041fc0c21cc9bb0bcefc56dd8ed51629
arnob <farhanasfar21@gmail.com>
2025-05-03 19:37:07 +0600

    export {app}:reason to not hit the route

--- app.js
+++ app.js
@@ -12,7 +12,16 @@ app.use(cookieParser());
 app.use(express.static("public"));
 
 
-import { userRouter } from "./routes/user.routes.js";
+// import { userRouter } from "./routes/user.routes.js";
 import { authRouter } from "./routes/auth.routes.js";
 
 app.use("/api/v1/auth", authRouter);
+// app.use("/api/v1/user", userRouter);
+
+app.post("/test-post", (req, res) => {
+    console.log("Test route hit", req.body);
+    res.json({ message: "Post request received", body: req.body });
+  });
+
+
+export {app};
