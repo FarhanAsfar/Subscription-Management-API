9440834bc8b6f1b6c7cd82136eab012b8c7971b7
arnob <farhanasfar21@gmail.com>
2025-05-06 22:11:29 +0600

    Add 'generateAccessToken' method

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,7 @@
-import mongoose from "mongoose";
-import bcryptjs from "bcryptjs";
+import mongoose from "mongoose"
+import bcryptjs from "bcryptjs"
+import jwt from "jsonwebtoken"
+
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
@@ -41,6 +43,20 @@ userSchema.methods.isPassword = async function (password) {
         console.log("Invalid password");
     }
     return await bcryptjs.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            username: this.username,
+            email: this.email
+        }, 
+        process.env.ACCESS_TOKEN_SECRET, 
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
 }
 
 const User = mongoose.model("User", userSchema);
