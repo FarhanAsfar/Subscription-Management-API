7eb94957a978654283ba21df93cdb43f842d9433
arnob <farhanasfar21@gmail.com>
2025-05-07 19:37:47 +0600

    Add signout route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
-import { signIN, signUP } from "../controllers/auth.controller.js";
+import { signIN, signUP, signOUT } from "../controllers/auth.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const authRouter = Router();
 
 authRouter.route("/signup").post(signUP);
 authRouter.route("/signin").post(signIN);
+authRouter.route("/signout").post(verifyJWT, signOUT);
 
-//authRouter.route("/signin").post(signIN);
 
 
 export {authRouter}
