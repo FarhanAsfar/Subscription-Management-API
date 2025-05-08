ca4312327cd49c223d089b0e7f04d25c8f27626f
arnob <farhanasfar21@gmail.com>
2025-05-08 23:47:18 +0600

    add route for api-doc

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import swaggerUi from 'swagger-ui-express';
+import { apiSpec } from './docs/openapi.js';
+
 // import { userRouter } from "./routes/user.routes.js";
 import { authRouter } from "./routes/auth.routes.js";
 
@@ -17,8 +20,12 @@ app.use(express.static("public"));
 
 app.use("/api/v1/auth", authRouter);
 
-app.get("/", (req, res) => {
-  res.send("Subscription Management Api");
+// Mount the Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(apiSpec));
+
+// Redirect root to docs
+app.get('/', (req, res) => {
+  res.redirect('/api-docs');
 });
 
 
