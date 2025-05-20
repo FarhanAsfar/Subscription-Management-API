87621769fd493d8ab508789872ce3963643dcf06
arnob <farhanasfar21@gmail.com>
2025-05-20 11:03:05 +0600

    Add 'changeUserPassword' route and controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -18,6 +18,49 @@ const getUserById = asyncHandler(async (req, res) => {
     )
 });
 
+const updateUserAccount = asyncHandler(async (req, res) => {
+    const userId = req.params.id;
+    const {username, email} = req.body;
+    const user = await User.findById(userId).select("-password");
+
+    if(!user){
+        throw new ApiError(404, "User not found")
+    }
+
+    if(!username && !email){
+        throw new ApiError(400, "Nothing was changed")
+    }
+
+    user.username = username;
+    user.email = email;
+    await user.save();
+
+    return res.status(200).json(
+        new ApiResponse(200, user, "User account updated successfully")
+    )
+})
+
+const changeUserPassword = asyncHandler(async (req, res) => {
+    const {oldPassword, newPassword} = req.body;
+
+    const user = await User.findById(req.user?._id);
+    const isPassword = await user.isPassword(oldPassword);
+
+    if(!isPassword){
+        throw new ApiError(400, "Password is not correct")
+    }
+
+    user.password = newPassword;
+    await user.save({validateBeforeSave: false});
+
+    return res.status(200).json(
+        new ApiResponse(200, {}, "Password changed successfully")
+    )
+})
+
+
 export {
     getUserById,
+    updateUserAccount,
+    changeUserPassword,
 }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,15 +1,16 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { getUserById } from "../controllers/user.controller.js";
+import { changeUserPassword, getUserById, updateUserAccount } from "../controllers/user.controller.js";
 
 const userRouter = Router();
 //route: api/v1/user
 
-// userRouter.route("/:id").get(getUserSubscriptions);
 
 userRouter.route("/:id").get(verifyJWT, getUserById);
 
-// userRouter.route("/:id").put(updateUser);
+userRouter.route("/update-account/:id").put(verifyJWT, updateUserAccount);
+
+userRouter.route("/change-password").put(verifyJWT, changeUserPassword);
 
 
 export {userRouter}
