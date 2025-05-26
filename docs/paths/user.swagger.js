0978b23b260b03f01594e8d25e00ab3cde7d016b
arnob <farhanasfar21@gmail.com>
2025-05-26 14:23:38 +0600

    Add update account doc

--- docs/paths/user.swagger.js
+++ docs/paths/user.swagger.js
@@ -40,4 +40,67 @@ export const userPaths = {
       }
     }
   },
+  //update user account
+  "/api/v1/user/update-account": {
+    put: {
+      tags: ["User"],
+      summary: "Update account of the logged in user",
+      security: [{ bearerAuth: [] }],
+      requestBody: {
+        required: true,
+        content: {
+          "application/json": {
+            schema: {
+              type: "object",
+              required: ["username", "email"],
+              properties: {
+                username: {
+                  type: "string",
+                  example: "John Doe",
+                },
+                email: {
+                  type: "string",
+                  format: "email",
+                  example: "john-new@gmail.com",
+                },
+              }
+            }
+          }
+        }
+      },
+      responses: {
+        200: {
+          description: "Account updated successfully",
+          content: {
+            "application/json": {
+              schema: {
+                type: "object",
+                properties: {
+                  statusCode: {
+                    type: "integer",
+                    example: 200
+                  },
+                  data: {
+                    type: "object",
+                    properties: {
+                      _id: { type: "string", example: "66328d1276d4cf89eb123456" },
+                      username: { type: "string", example: "John New" },
+                      email: {type: "string", format:"email", example: "updated-john@gmail.com"},
+                    }
+                  },
+                  message: {
+                    type: "string",
+                    example: "Account updated successfully"
+                  }
+                }
+              }
+            }
+          }
+        },
+        400: {
+          description: "Could not update account"
+        }
+      }
+    }
+  },
 }
