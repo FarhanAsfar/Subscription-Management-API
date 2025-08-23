b3c95f5b56dc52828f62c2f57be1b35f23ef90fd
arnob <farhanasfar21@gmail.com>
2025-08-23 07:59:03 +0600

    Add route to get all users info for admin

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { createAdmin, loginAdmin} from "../controllers/admin.controller.js";
+import { createAdmin, loginAdmin, usersInfo} from "../controllers/admin.controller.js";
 import { isAdmin } from "../middlewares/auth.middleware.js";
 
 const adminRouter = Router();
 
-
 adminRouter.route("/create-admin").post(isAdmin, createAdmin);
 adminRouter.route("/login-admin").post(loginAdmin);
+adminRouter.route("/get-all-users").get(isAdmin, usersInfo);
 
 // Initial admin info
 // adminName: admin me
